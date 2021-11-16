5655553928d676e27fa9d30472edb61b255471d9
Arial02 <53966320+Arial02@users.noreply.github.com>
2021-11-16 19:51:52 +0300

    Ready

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-import pyxel
 from random import randint
 import numpy as np
-from time import sleep, time
+from time import sleep
 import keyboard
-########## DEBUG ############
+import curses, sys
+from threading import Thread
+from functools import reduce
 
-counter, beg, end=0,0,0
 
-# на 50 строчке функция pyxel.run(...) сбоит
-# должна работать так: UPDATE DRAW UPDATE DRAW UPDATE DRAW UPDATE DRAW
-# работает так: UPDATE DRAW UPDATE DRAW UPDATE UPDATE UPDATE UPDATE UPDATE DRAW UPDATE UPDATE UPDATE...
-
-##############################
-
+def prod(iterable):
+    return reduce(lambda x, y: x*y, iterable, 1)
 
 def swap_rows(arr, k, r):
     for i in range(len(arr[k])):
@@ -20,7 +16,7 @@ def swap_rows(arr, k, r):
 
 ACCELERATION = 0.99
 
-COLOR = {0: 0, 1: 8, 2: 11} # обозначение на карте: обозначение цвета
+COLOR = {0: "◦", 1: "■", 2: "□"} # обозначение на карте: обозначение цвета
 
 UP = (0, 1)
 RIGHT = (1, 0)
@@ -39,7 +35,12 @@ TURNING = ((0,-1),(1,0))
 
 class Tetris:
     def __init__(self):
-        pyxel.init(10, 20, caption="Tetris")
+        self.stdscr = curses.initscr()
+        self.hook = keyboard.hook(lambda event: self.events.append(event))
+        self.events=[]
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(True)
         self.map=[[0 for j in range(10)] for i in range(23)] # 3 rows in the top are hidden
         self.timing=0.6
         self.init_pos=4
@@ -47,95 +48,119 @@ class Tetris:
         self.death=False
         self.pause=False
         self.reset()
-        pyxel.run(self.update, self.draw) # эта функция описана в ./Lib/site-packages/pyxel/__init__.py на ~560 строке
-        """while True:
-            try:
-                self.update()
-            except:
-                print("ERROR UPDATE")
-            try:
-                self.draw()
-            except:
-                print("ERROR DRAW")""" # эквивалентно ли? Смущает блок core.run(...)
+
+    def __del__(self):
+        self.stdscr.clear()
+        self.stdscr.refresh()
+        curses.nocbreak()
+        self.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+        keyboard.unhook_all()
+
+    def reborn(self):
+        self.stdscr = curses.initscr()
+        self.hook = keyboard.hook(lambda event: self.events.append(event))
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(True)
 
     def reset(self):
-        print("(RESET)", end=" ")
         self.is_pull=False
         self.is_fast=False
         ready_rows=0
         self.timing*=ACCELERATION
-        self.dir = UP
         self.kind=list(FIGURES.keys())[randint(0,6)]
         self.obj = FIGURES[self.kind]
-        self.obj = [[self.obj[i][j] + RIGHT[j]*self.init_pos for j in range(2)] for i in range(4)]
+        self.obj = [[self.obj[i][j] + RIGHT[j]*self.check_init_pos() for j in range(2)] for i in range(4)]
         if self.kind!="I":
             self.obj = [[self.obj[i][j] + DOWN[j] * 2 for j in range(2)] for i in range(4)]
         for i in range(len(self.obj)):
             self.map[-self.obj[i][1]][self.obj[i][0]]=2
         for k in range(3,len(self.map),1):
-            if sum(self.map[i])>=10:
+            if prod(self.map[k]):
                 self.map[k]=[0 for i in range(len(self.map[k]))]
                 ready_rows+=1
                 for i in range(k,0,-1):
                     swap_rows(self.map, i, i-1)
-        self.score+=100*(2*ready_rows-1)
+        if ready_rows:
+            self.score+=100*(2*ready_rows-1)
         if sum([1 if self.map[3][k]==1 else 0 for k in range(len(self.map[3]))]):
             self.death=True
 
+    def check_init_pos(self):
+        if self.kind=="O":
+            return self.init_pos if self.init_pos!=9 else 8
+        elif self.kind!="I":
+            return self.init_pos if self.init_pos<8 else 7
+        else:
+            return self.init_pos
 
     def update(self):
-        print("UPDATE",end=" = ")
-        beg=time()
-        if pyxel.btnp(pyxel.KEY_Q): # кнопки не отдебажены! Дебаг после разрешения проблемы с 50 строчкой
-            pyxel.quit()
-        if pyxel.btnp(pyxel.KEY_P):
-            self.pause=True # keyboard.wait("p") в отрисовке экрана паузы
-        if pyxel.btnp(pyxel.KEY_RIGHT):
-            self.obj = [[self.obj[i][j] + RIGHT[j] for j in range(len(self.obj[i]))] for i in range(len(self.obj))]
-        if pyxel.btnp(pyxel.KEY_LEFT):
-            self.obj = [[self.obj[i][j] + LEFT[j] for j in range(len(self.obj[i]))] for i in range(len(self.obj))]
-        if pyxel.btnp(pyxel.KEY_SPACE):
-            self.is_pull=True
-        if pyxel.btnp(pyxel.KEY_UP):
-            shift = (self.obj[0][0],self.obj[0][1])
-            shift[0]-=1 if self.kind=="S" else 0
-            backup = ((self.obj[i][j] for j in range(len(self.obj[i]))) for i in range(len(self.obj)))
-            for i in range(len(self.obj)):
-                self.map[-self.obj[i][1]][self.obj[i][0]] = 0
-            for i in range(len(self.obj)):
-                self.obj[i][0]-=shift[0]
-                self.obj[i][1]-=shift[1]
-            if self.dir==UP: self.dir=RIGHT
-            elif self.dir==RIGHT: self.dir=DOWN
-            elif self.dir==DOWN: self.dir=LEFT
-            elif self.dir==LEFT: self.dir=UP
-            self.obj = tuple(map(lambda x: tuple(x), (np.array(self.obj) @ np.array(TURNING)).tolist()))
-            for i in range(len(self.obj)):
-                self.obj[i][0]+=shift[0]
-                self.obj[i][1]+=shift[1]
-            for k in range(len(self.obj)):
-                if self.obj[k][0]<0:
+        for event in self.events:
+            if event.name == "q" and event.event_type == "down":
+                self.__del__()
+                print("You loose, man!\nYou got {} points.\n".format(self.score))
+                sys.exit(0)
+            if event.name == "p" and event.event_type == "down":
+                self.pause = True
+            if event.name == "right" and event.event_type == "down":
+                if not sum([1 if self.obj[k][0]==9 or self.map[-self.obj[k][1]][self.obj[k][0]+1]==1 else 0 for k in range(len(self.obj))]):
                     for i in range(len(self.obj)):
-                        self.obj[i][0]-=self.obj[k][0]
-                elif self.obj[k][0]>9:
+                        self.map[-self.obj[i][1]][self.obj[i][0]] = 0
+                    self.obj = [[self.obj[i][j] + RIGHT[j] for j in range(len(self.obj[i]))] for i in range(len(self.obj))]
                     for i in range(len(self.obj)):
-                        self.obj[i][0]-=(self.obj[k][0]-9)
-                if self.obj[k][1]<0:
+                        self.map[-self.obj[i][1]][self.obj[i][0]] = 2
+            if event.name == "left" and event.event_type == "down":
+                if not sum([1 if self.obj[k][0]==0 or self.map[-self.obj[k][1]][self.obj[k][0]-1]==1 else 0 for k in range(len(self.obj))]):
                     for i in range(len(self.obj)):
-                        self.obj[i][1]-=self.obj[k][1]
-                elif self.obj[k][1]>22:
+                        self.map[-self.obj[i][1]][self.obj[i][0]] = 0
+                    self.obj = [[self.obj[i][j] + LEFT[j] for j in range(len(self.obj[i]))] for i in range(len(self.obj))]
                     for i in range(len(self.obj)):
-                        self.obj[i][1]-=(self.obj[k][1]-22)
-            for k in range(len(self.obj)):
-                if self.map[-self.obj[k][1]][self.obj[k][0]]==1:
-                    self.obj=((backup[i][j] for j in range(len(backup[i]))) for i in range(len(backup)))
-                    break
-            for i in range(len(self.obj)):
-                self.map[-self.obj[i][1]][self.obj[i][0]] = 2
-        if pyxel.btnp(pyxel.KEY_DOWN):
-            self.is_fast=True
-        if pyxel.btnr(pyxel.KEY_DOWN):
-            self.is_fast=False
+                        self.map[-self.obj[i][1]][self.obj[i][0]] = 2
+            if event.name == "space" and event.event_type == "down":
+                self.is_pull = True
+            if event.name == "up" and event.event_type == "down":
+                shift = [self.obj[0][0], self.obj[0][1]]
+                shift[0] -= 1 if self.kind == "S" else 0
+                backup = [[self.obj[i][j] for j in range(len(self.obj[i]))] for i in range(len(self.obj))]
+                for i in range(len(self.obj)):
+                    self.map[-self.obj[i][1]][self.obj[i][0]] = 0
+                for i in range(len(self.obj)):
+                    self.obj[i][0] -= shift[0]
+                    self.obj[i][1] -= shift[1]
+                self.obj = (np.array(self.obj) @ np.array(TURNING)).tolist()
+                for i in range(len(self.obj)):
+                    self.obj[i][0] += shift[0]
+                    self.obj[i][1] += shift[1]
+                for k in range(len(self.obj)):
+                    if self.obj[k][0] < 0:
+                        chg = self.obj[k][0]
+                        for i in range(len(self.obj)):
+                            self.obj[i][0] -= chg
+                    elif self.obj[k][0] > 9:
+                        chg = (self.obj[k][0] - 9)
+                        for i in range(len(self.obj)):
+                            self.obj[i][0] -= chg
+                    if -self.obj[k][1] < 0:
+                        chg = self.obj[k][1]
+                        for i in range(len(self.obj)):
+                            self.obj[i][1] -= chg
+                    elif -self.obj[k][1] > 22:
+                        chg = (self.obj[k][1] + 22)
+                        for i in range(len(self.obj)):
+                            self.obj[i][1] -= chg
+                for k in range(len(self.obj)):
+                    if self.map[-self.obj[k][1]][self.obj[k][0]] == 1:
+                        self.obj = ((backup[i][j] for j in range(len(backup[i]))) for i in range(len(backup)))
+                        break
+                for i in range(len(self.obj)):
+                    self.map[-self.obj[i][1]][self.obj[i][0]] = 2
+            if event.name == "down" and event.event_type == "down":
+                self.is_fast = True
+            if event.name == "down" and event.event_type == "up":
+                self.is_fast = False
+        self.events.clear()
         if sum([1 if -self.obj[i][1]==22 else 0 for i in range(len(self.obj))]):
             for i in range(len(self.obj)):
                 self.map[-self.obj[i][1]][self.obj[i][0]] = 1
@@ -152,26 +177,37 @@ class Tetris:
             self.obj = [[self.obj[i][j]+DOWN[j] for j in range(len(self.obj[i]))] for i in range(len(self.obj))]
             for i in range(len(self.obj)):
                 self.map[-self.obj[i][1]][self.obj[i][0]] = 2
-        end = time()
-        print(end-beg,"s")
 
 
     def draw(self):
-        print("DRAW")
-        pyxel.cls(0)
         if self.death:
-            # Тут должен был быть экран смерти
+            self.__del__()
+            print("You loose, man!\nYou got {} points.\n(Press Q)\n".format(self.score))
             keyboard.wait("q")
-            pyxel.quit()
+            sys.exit(0)
         elif self.pause:
-            # Тут должен был быть экран паузы
+            flag = False
+            self.__del__()
+            t = Thread(target=self.pauser, args=(lambda: flag,))
+            t.start()
             keyboard.wait("p")
             self.pause=False
+            flag=True
+            t.join()
+            self.reborn()
         else:
-            pyxel.clip() # в перспективе можно где-нибудь внизу записать очки, цифры 3*5рx
-            for i in range(3,len(self.map),1):
-                for j in range(len(self.map[i])):
-                    pyxel.pset(j,i-3,COLOR[self.map[i][j]])
+            try:
+                self.stdscr.addstr(" "+"_"*19+"\n")
+                for i in range(3,len(self.map),1):
+                    self.stdscr.addstr("|"+" ".join(list(map(lambda x: COLOR[x], self.map[i]))) + "|\n")
+                self.stdscr.addstr(" "+"̅"*19+"\n")
+                self.stdscr.addstr(" "*2+"You got {} points!".format(self.score))
+                self.stdscr.refresh()
+                self.stdscr.clear()
+            except curses.error:
+                self.__del__()
+                print("The console must be open with at least 23 visible free lines!")
+                sys.exit(1)
         if self.is_pull:
             sleep(0.034)
         elif self.is_fast:
@@ -179,6 +215,30 @@ class Tetris:
         else:
             sleep(self.timing)
 
-Tetris()
+    def pauser(self, stop):
+        while True:
+            for i in range(4):
+                sys.stdout.write("\rPause" + "." * i + " " * (3-i))
+                sys.stdout.flush()
+                sleep(0.5)
+            if stop():
+                break
+
+def game_loop(game):
+    while True:
+        try:
+            game.update()
+        except Exception:
+            game.__del__()
+            print("UPDATE ERROR")
+            sys.exit(1)
+        try:
+            game.draw()
+        except Exception:
+            game.__del__()
+            print("DRAW ERROR")
+            sys.exit(1)
+
+game_loop(Tetris())
 # отрисовка на карте с минусом в игрике, т.к. у фигуры ось игрик вверх, у карты - вниз
 # upd оси походу тоже попутаны, ахах, итого map[-obj.y][obj.x] <- map.x & map.y
